dc6f8ad5ca6d9d62d4dbec70a913991e5f9386fa
szilard-molnar <mszilard86@gmail.com>
2019-09-06 23:46:38 -0400

    selecting items, counting costs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host:"localhost",
     port:"8889",
@@ -7,6 +8,8 @@ var connection = mysql.createConnection({
     database:"bamazon"
 });
 
+
+function allProducts() {
 connection.connect(function(error){
     if(error)
     {
@@ -33,3 +36,60 @@ connection.connect(function(error){
         })
     }
 })
+}
+
+allProducts();
+
+
+function buyProduct() {
+    inquirer.prompt([
+        {
+            type:"input",
+            name: "id",
+            message: "Enter the ID of the product you would like to order."
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many of this products you would like to order?"
+        }
+    ])
+    .then(function(response){
+        itemRequested = response.id;
+        quantityRequested = parseInt(response.quantity)
+        buyThisItem(itemRequested, quantityRequested)
+    })
+
+    function buyThisItem(itemRequested, quantityRequested) {
+        connection.query("SELECT * FROM products where item_id=" + itemRequested,function(error, res){
+            if(error)
+            {
+                console.log(error);
+            }
+            else
+            {
+                for(var i = 0; i < res.length; i++)
+                {
+                    var numberOfItems = parseInt(res[i].stock_quantity);
+                    console.log("Currently available: " + numberOfItems);
+                    console.log("Quantity requested: " + quantityRequested);
+                    if(numberOfItems < quantityRequested)
+                    {
+                        console.log("Insufficient quantity...");
+                        buyProduct();
+                    }
+                    else
+                    {
+                        numberOfItems-=quantityRequested;
+                        console.log("Amount of items left in stock after purchase: " + numberOfItems);
+
+                        var totalCost = res[i].price * quantityRequested;
+                        console.log("Total cost: $" + totalCost);
+                    }
+                }
+            }
+        })
+    }
+}
+
+buyProduct();
