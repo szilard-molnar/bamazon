b50689edda1b5dd71e2b9d4a04e924b8a746643f
szilard-molnar <mszilard86@gmail.com>
2019-09-06 22:41:46 -0400

    I can get certain information from the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,14 @@ connection.connect(function(error){
                 console.log(error);
             }
             else{
-                console.log(res);
+                //console.log(res);
+                for(var i = 0; i < res.length; i++)
+                {
+                    console.log("Product ID: ",res[i].item_id);
+                    console.log("Product name: ",res[i].product_name);
+                    console.log("Price: ",res[i].price);
+                    console.log("-----------------------");
+                }
             }
         })
     }
